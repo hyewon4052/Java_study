79bea35b4eb3faff1d0eb039cf0e7793df7c3fa5
hyewon4052 <s2311@e-mirim.hs.kr>
2023-05-26 10:02:31 +0900

    Exam_05

---
+++ Exam_05.java
@@ -0,0 +1,13 @@
+class Exam_05
+{
+	public static void main(String[] args) 
+	{
+		short a;
+		a = 10;
+		a = 15;		//변수
+
+		short b, c = (20 + 5) * 2;	//연산자 우선 순위
+		System.out.println(c);
+
+	}
+}
