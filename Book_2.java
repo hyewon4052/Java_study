24ef32caa802abc58ee64a91676018204130da7b
hyewon4052 <s2311@e-mirim.hs.kr>
2023-05-26 09:32:02 +0900

    book-2

---
+++ Book_2.java
@@ -0,0 +1,22 @@
+class  Book_2
+{
+	public static void main(String[] args) 
+	{
+//		int a = 20;
+//		int b = 3;
+//		System.out.println("a + b = " + (a+b));
+//		System.out.println("a - b = " + (a-b));
+//		System.out.println("a * b = " + (a*b));
+//		System.out.println("a / b = " + (a/b));
+//		System.out.println("a % b = " + (a%b));
+
+int a = 1, b =2;
+System.out.println((a>b) & (a<b));
+System.out.println((a>b) && (a<b));
+System.out.println((a<b) | (a>b));
+System.out.println((a<b) || (a>b));
+System.out.println(!(a<b));
+
+
+	}
+}
