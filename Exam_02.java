0019515872cf3601ec1949aa405fc25cc5330351
hyewon4052 <s2311@e-mirim.hs.kr>
2023-05-26 09:36:21 +0900

    Exam_02

---
+++ Exam_02.java
@@ -0,0 +1,11 @@
+class Exam_02 
+{
+	public static void main(String[] args) 
+	{
+		char ch = 'A';	//char ch = '\u0041';
+		System.out.println(ch);
+		
+		int code=(int)ch;
+		System.out.println(code);
+	}
+}
