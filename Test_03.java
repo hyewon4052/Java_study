ef2cc9750d96471bb47df4a45f192ac4b6888009
hyewon4052 <s2311@e-mirim.hs.kr>
2023-05-26 10:29:48 +0900

    Test_03

---
+++ Test_03.java
@@ -0,0 +1,19 @@
+public class Test_03 {
+	public static void main(String args[]) {
+
+		String str1 = "미스터트롯2";
+		String str2 = "김용필";
+ 		System.out.print(str1 + ":");
+		System.out.print(str2);
+		System.out.println();
+
+		int a = 48;
+		System.out.println(str1 = " " + str2 + "의 나이는 " + a + "세 입니다.");
+
+		a = 49;
+		System.out.println("내년에 " +	str2 + "의 나이는 " + a + "세 입니다.");
+
+
+	}
+
+}
