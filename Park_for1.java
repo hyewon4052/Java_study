a3cecc1d5c4097b8efd452c5cb493c1f7e4b13db
hyewon4052 <s2311@e-mirim.hs.kr>
2023-05-26 10:21:32 +0900

    Park_for1

---
+++ Park_for1.java
@@ -0,0 +1,17 @@
+class Park_for1 
+{
+	public static void main(String[] args) 
+	{
+		int gu = 2;
+
+		for (gu = 2 ; gu < 10; gu++ )
+		{
+			if (gu % 2 == 0)
+				for (int i =1; i<10 ; i++)
+				{
+					System.out.print(gu + " * " + i +  " = " + gu*i + "\t");
+				}
+			System.out.println("");
+		}
+	}
+}
