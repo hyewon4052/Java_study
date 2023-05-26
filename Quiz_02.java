ff61e600ff4876bf867eab1cecf717befbd53782
hyewon4052 <s2311@e-mirim.hs.kr>
2023-05-26 10:28:04 +0900

    Quiz_02

---
+++ Quiz_02.java
@@ -0,0 +1,11 @@
+public class Quiz_02 {
+	public static void main (String args[]) {
+				
+		String name = "조갑경";
+		int age = 55;
+
+
+
+		System.out.println("내가 좋아하는 연예인은 " + name + "이고 " + (age+1) + "세 입니다.");
+	}
+}
