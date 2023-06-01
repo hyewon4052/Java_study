c5e0490145ff3846c489aafb5336b3178b538546
hyewon4052 <s2311@e-mirim.hs.kr>
2023-06-02 01:09:16 +0900

    Exam_05

--- Exam_05.java
+++ Exam_05.java
@@ -2,12 +2,90 @@ class Exam_05
 {
 	public static void main(String[] args) 
 	{
-		short a;
-		a = 10;
-		a = 15;		//변수
+//		short a;
+//		a = 10;
+//		a = 15;		//변수
+//
+//		short b, c = (20 + 5) * 2;	//연산자 우선 순위
+//		System.out.println(c);
 
-		short b, c = (20 + 5) * 2;	//연산자 우선 순위
-		System.out.println(c);
+//		byte ba;
+//		ba = (byte)1000;			// 강제형 변환
+//		System.out.println(ba);
 
-	}
+//		char ch = 'a';
+//		System.out.println(ch);
+//		System.out.println("a는 " + (int)ch);
+
+//		int yy = 97;
+//		System.out.println((char)yy);
+//		System.out.println((int)'a');
+
+//		boolean zz;
+//		zz = false;
+//		System.out.println(zz);
+
+//		int ib = 100;			// 정수형의 기본형 int
+//		long la = 100L;	
+//		float fa = 5.3F;		//실수형의 기본형 double
+//		float fb = (float)5.3;	//강제형 변환 : 손실 가능성 주의
+ 
+//		int ic = (int)5.8;			// 강제형 변환
+//		System.out.println(ic);		//손실 가능성에 주의
+
+//		double db = 4.5 + 10;		//자동형 변환
+//		System.out.println(cd);
+		
+		//증감연산자 : ++a, a++, --a,a--
+//		int id = 1;
+//		Systerm.out.println("id + = " + id);
+//		int ie = id++;		//(id = id + 1);
+//		System.out.println("id = " + id + ", ie = " + ie);
+
+		
+//		int ih = 10 / 3;		// 몫
+//		int ii = 10 % 3;		//나머지
+//
+//		System.out.println("ih = " + ih);
+//		System.out.println("ii = " + ii);
+		
+		int ij, ik;
+		ij = 8;
+
+//		ik = ij << 1;
+//		System.out.println("ik = " + ik);
+
+//		ik = ij >> 1;
+//		System.out.println("ik = " + ik);
+		
+//		ik = ij >>> 1;			//왼쪽은 무조건 0으로 채움
+//		System.out.println("ik = " + ik);
+		
+		// 삼항 연산자
+//		(a>b) ? a = 1 : b = 1;
+//
+//			if (a>b)
+//			{
+//				a = 1;
+//			}
+//			else 
+//			{
+//				b = 1;
+//			}
+
+			int m=10, n;
+//			n = ( (m == 5) || (m >= 7) ) ? ++m : 200;
+//			System.out.println(n);
+
+			if ((m == 5) || (m >= 7))
+			{
+				n = ++m;
+			}
+			else 
+			{
+				n = 200;
+			}
+			System.out.println(n);
+			
+		}
 }
