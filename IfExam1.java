283ece69744bf30d5ad63c8f1138ff597f1ca3ee
hyewon4052 <s2311@e-mirim.hs.kr>
2023-06-15 20:04:52 +0900

    IfExam1

---
+++ IfExam1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class IfExam1 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수 입력 : ");
+		int a = sc.nextInt();
+//		if (a % 2 == 0)	{
+//			System.out.println(a + "는 짝수입니다.");
+//		}
+//		else { 
+//			System.out.println(a + "는 홀수입니다.");
+//		}	
+
+		String result = (a % 2 == 0) ? "짝수" : "홀수"; 
+			System.out.println( a + "는 " + result + "입니다.");
+	}
+}
