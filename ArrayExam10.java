161548a91c5f49af2b100da4c17db63086193a10
hyewon4052 <s2311@e-mirim.hs.kr>
2023-07-27 23:45:21 +0900

    ArrayExam10

---
+++ ArrayExam10.java
@@ -0,0 +1,21 @@
+class ArrayExam10 
+{
+	public static void main(String[] args) 
+	{
+		// 1. 선언
+			int a[][];
+			int[][] a;
+			int[] a[];
+
+		// 2. 생성
+			a = new int[2][3];
+
+		// 3. 초기화
+			a[0][0] = 1;
+			a[0][1] = 2;
+			a[0][2] = 3;
+			a[1][0] = 10;
+			a[1][1] = 20;
+			a[1][2] = 30;
+	}
+}
