40a447542ed0135b6170c746d5a7c4154231a76a
hyewon4052 <s2311@e-mirim.hs.kr>
2023-07-28 23:57:35 +0900

    ArrayExam9

---
+++ ArrayExam9.java
@@ -0,0 +1,23 @@
+class ArrayExam9 
+{
+	public static void main(String[] args) 
+	{
+		int a[];
+		int b[];
+		int c[] = {31,32,33};
+		a = new int[4];
+		b = new int[] {21,22,23,34};
+
+		System.out.println(a.length);
+		System.out.println(b.length);
+		System.out.println(c.length);
+
+		c = b;	// call by reference여서 주소값을 넘겨줌
+		
+		System.out.println(a.length);
+		System.out.println(b.length);
+		System.out.println(c.length);
+
+		
+	}
+}
