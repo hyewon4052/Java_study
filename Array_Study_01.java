37d862ab94e753f1749899376b89aac651eb120a
hyewon4052 <s2311@e-mirim.hs.kr>
2023-08-03 22:49:58 +0900

    Array_Study_01

---
+++ Array_Study_01.java
@@ -0,0 +1,20 @@
+class Array_Study_01 
+{
+	public static void main(String[] args) 
+	{
+		int score[] = {100, 88, 100, 100, 90};
+		int sum = 0;
+		float average;
+
+		for (int i = 0; i < score.length ; i++)
+		{
+			sum += score[i];
+		}
+
+		average = sum / (float) score.length;
+		System.out.println("총점 = " + sum);
+		System.out.println("평균 = " + average);
+
+		
+	}
+}
