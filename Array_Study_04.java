c889cef938a0557e5d6b2a01c6eb372ddf1716e4
hyewon4052 <s2311@e-mirim.hs.kr>
2023-08-04 23:17:43 +0900

    Array_Study_04

--- Array_Study_04.java
+++ Array_Study_04.java
@@ -2,13 +2,24 @@ class Array_Study_04
 {
 	public static void main(String[] args) 
 	{
-		int table[][] = new int[3][4];
-		table[0][0] = 10;
-		table[1][1] = 20;
-		table[2][3] = table[0][0] + table[1][1];
+//		int table[][] = new int[3][4];
+//		table[0][0] = 10;
+//		table[1][1] = 20;
+//		table[2][3] = table[0][0] + table[1][1];
+//
+//		System.out.println(table[0][0]);
+//		System.out.println(table[1][1]);
+//		System.out.println(table[2][3]);
 
-		System.out.println(table[0][0]);
-		System.out.println(table[1][1]);
-		System.out.println(table[2][3]);
+		int table[][] = { {1}, {2,3}, {4,5,6,7,8} };
+		for (int i = 0; i < table.length ; i++ )
+		{
+			for (int j = 0 ; j < table[i].length ; j++)
+			{
+				System.out.print(table[i][j] + "\t");
+			} // 서술형 기말고사 / 수행평가 시험문제
+			System.out.println();
+		}
+
 	}
 }
