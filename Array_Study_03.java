bb60092024843a1c0eb6d8a0130f10ccd015e858
hyewon4052 <s2311@e-mirim.hs.kr>
2023-12-22 20:00:00 +0900

    init commit

--- Array_Study_03.java
+++ Array_Study_03.java
@@ -3,7 +3,7 @@ class Array_Study_03
 	public static void main(String[] args) 
 	{
 		int ballArr[] = {1,2,3,4,5,6,7,8,9};
-		int ball3[] =new int [3];
+		int ball3[] =new int [3]; 
 		int temp = 0;
 
 		for (int x = 1; x <= 10; x++)
