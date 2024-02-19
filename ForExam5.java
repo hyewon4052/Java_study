8133100924b45d17cd3689833047c0caf5ba01d4
hyewon4052 <s2311@e-mirim.hs.kr>
2024-02-19 20:00:00 +0900

    init commit

--- ForExam5.java
+++ ForExam5.java
@@ -1,4 +1,4 @@
-class ForExam5 
+ class ForExam5 
 {
 	public static void main(String[] args) 
 	{
@@ -6,7 +6,7 @@ class ForExam5
 		{
 			for (int j = 1 ; j <= i; j++ )
 			{
-				System.out.print(j + " ");
+				System.out.print(j + "  ");
 			}
 			System.out.println();
 		}
